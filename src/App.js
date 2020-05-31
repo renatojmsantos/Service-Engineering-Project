2f0acf146f9d3330a56b0bd5040a4b976ffdbc7d
FranciscoSantiago15 <57218261+FranciscoSantiago15@users.noreply.github.com>
2020-05-31 02:21:05 +0100

    v3
     (Record/Stop) -> record saved in s3 -> lambda function get audio -> send to transcribe

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import './App.css';
 import MicRecorder from 'mic-recorder-to-mp3';
+import AWS from 'aws-sdk';
 
 const Mp3Recorder = new MicRecorder({ bitRate: 128 });
+
+
+var bucketName = "proj-es-s3/Voice";
+var bucketRegion = "us-east-1";
+var IdentityPoolId = "us-east-1:4797cc31-91b9-4466-8732-8e4707b5cfbc";
+
+AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: IdentityPoolId
+  })
+});
+
+
+var s3 = new AWS.S3({
+  apiVersion: "2006-03-01",
+  params: { Bucket: bucketName }
+});
+
 
 class App extends React.Component {
   constructor(props){
@@ -33,6 +53,8 @@ class App extends React.Component {
       .then(([buffer, blob]) => {
         const blobURL = URL.createObjectURL(blob)
         this.setState({ blobURL, isRecording: false });
+
+        this.addAudio(blob)
       }).catch((e) => console.log(e));
   };
 
@@ -47,6 +69,34 @@ class App extends React.Component {
         this.setState({ isBlocked: true })
       },
     );
+  }
+
+  addAudio = (audio) => {
+    console.log(audio)
+    var fileName = "audio.mp3";
+    if(!audio.size) {
+      return alert("No File to upload !!");
+    }
+    
+    var upload = new AWS.S3.ManagedUpload({
+      params: {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: audio
+      }  
+    });
+
+    var promise = upload.promise();
+
+    promise.then(
+      function(data) {
+        alert("Audio inserido na Storage");
+      },
+      function(err) {
+        return alert("ERROR: ", err.message);
+      }
+    )
+
   }
 
   render(){
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import LoginPage from './LoginPage';
+import Page from './counter';
 import App from './App';
-import Home from './uploadS3_v2';
+//import Home from './uploadS3_v2';
 //import Home from './uploadS3';
 import * as serviceWorker from './serviceWorker';
 
+
+
 ReactDOM.render(
-    <LoginPage loginserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/login" timeserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/time"/>,
+    //<Page loginserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/login" timeserver="https://4xfwd1debf.execute-api.us-east-1.amazonaws.com/test/time"/>,
+    <App />,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
