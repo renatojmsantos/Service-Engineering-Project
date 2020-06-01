76f571d4f495fe3b21d1972232c7a850fddd4090
FranciscoSantiago15 <57218261+FranciscoSantiago15@users.noreply.github.com>
2020-06-01 20:31:44 +0100

    v4

---
+++ src/getTranscribeData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class TranscribeData extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            body: '',
+        };
+    }
+
+    async componentDidMount() {
+        const url = "https://3xeam2g64j.execute-api.us-east-1.amazonaws.com/transcribe/transcribe";
+        const response = await fetch(url)
+        const data = await response.json();
+        console.log(data);
+        this.setState({ body: data.body })
+
+    }    
+
+
+    render() {
+        return (
+            <div>
+                {this.state.body}
+            </div>
+        )
+    }
+}
+
+export default TranscribeData;
---
+++ src/getTranscribeStatus.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import TranscribeData from './getTranscribeData'
+
+
+class TranscribeFetch extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            aux: 0,
+            body: '',
+            isLoading: true
+        };
+    }
+
+    async componentDidMount() {
+        const url = "https://3xeam2g64j.execute-api.us-east-1.amazonaws.com/transcribe/getstatus";
+        const response = await fetch(url)
+        const data = await response.json();
+        //console.log(data);
+        this.setState({ body: data.body })
+        console.log(this.state.body);
+
+        if(this.state.body === String('"IN_PROGRESS"')) {
+            this.setState({ aux: 0, isLoading: true })
+            this.componentDidMount();
+        } else {
+            /*if(this.state.aux === 0) {
+                console.log("1111")
+                this.setState({ aux:1 })
+                console.log(this.state.aux);
+            } else {
+                console.log("2222")
+                this.setState({ isLoading: false });
+            }    */
+            this.setState({ isLoading: false })      
+        }
+    }    
+
+    render() {
+        return (
+            <div>
+                {this.state.isLoading ? (
+                    <div>
+                        Loading...
+                    </div>                    
+                ) : (
+                    <div><TranscribeData /></div>
+                )}
+            </div>
+        )
+    }
+    
+}
+    
+
+
+export default TranscribeFetch;
+
